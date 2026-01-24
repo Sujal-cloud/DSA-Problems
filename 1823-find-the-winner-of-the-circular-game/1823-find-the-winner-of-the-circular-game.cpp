030ad490a5e41b150cd62b2a21ef125d2a94a6e2
Sujal Jethwani <sujaljethwani577@gmail.com>
2026-01-24 20:32:34 +0530

    Time: 0 ms (100%), Space: 7.8 MB (79.85%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        int ans = 0;
+        int i = 1;
+
+        while(i <= n) {
+            ans = (ans + k) % i;
+            i++;
+        }
+        return ans + 1;
+    }
+};
