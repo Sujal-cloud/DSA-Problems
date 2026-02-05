03b3ff16677f77bce6172cd8878b288b7a434a29
Sujal Jethwani <sujaljethwani577@gmail.com>
2026-02-05 20:14:08 +0530

    Time: 3 ms (89.69%), Space: 25.8 MB (8.08%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> result(n);
+        for(int i=0; i<n; i++) {
+            if(nums[i] == 0) {
+                result[i] = nums[i];
+            }else{
+                int jmp = nums[i] % n;
+                int idx = (i + jmp) % n;
+                if(idx < 0) idx += n;
+                result[i] = nums[idx];
+            }
+        }
+        return result;
+    }
+};
