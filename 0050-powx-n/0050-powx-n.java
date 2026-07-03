deb4becdbcc37915dbb91d8fb404473f9db9a02f
Sujal Jethwani <sujaljethwani577@gmail.com>
2026-07-03 08:45:57 +0530

    Time: 0 ms (100%), Space: 47.8 MB (71.82%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,20 @@
+class Solution {
+    static double solve(double x, long n) {
+        if(n == 0) {
+            return 1.00;
+        }
+        if(n < 0) {
+            return solve(1/x, -n);
+        }
+
+        double half = solve(x, n/2);
+        if(n % 2 == 0) {
+            return half * half;
+        }else{
+            return x * half * half;
+        }
+    }
+    public double myPow(double x, int n) {
+        return solve(x, (long)n); 
+    }
+}
