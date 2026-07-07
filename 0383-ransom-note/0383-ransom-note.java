7d0742d93f2ce366f01cfe4e3a146409f9fd8954
Sujal Jethwani <sujaljethwani577@gmail.com>
2026-07-07 09:55:15 +0530

    Time: 3 ms (51.83%), Space: 46.5 MB (49.69%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,20 +1,19 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> map1 = new HashMap<>();
+        int[] freq = new int[26];
 
-        for(char ch : magazine.toCharArray()) {
-            map1.put(ch, map1.getOrDefault(ch, 0) + 1);
+        
+        for(int i=0; i<magazine.length(); i++) {
+            char ch = magazine.charAt(i);
+            freq[ch - 'a']++;
         }
 
-        for(char ch : ransomNote.toCharArray()) {
-            if(map1.containsKey(ch)) {
-                map1.put(ch, map1.get(ch) - 1);
-
-                if(map1.get(ch) == 0) {
-                    map1.remove(ch);
-                }
-            }else {
+        for(int i=0; i<ransomNote.length(); i++) {
+            char ch = ransomNote.charAt(i);
+            if(freq[ch - 'a'] == 0) {
                 return false;
+            }else{
+                freq[ch - 'a']--;
             }
         }
         return true;
