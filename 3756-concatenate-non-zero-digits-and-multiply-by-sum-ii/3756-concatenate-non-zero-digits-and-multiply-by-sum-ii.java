9460eea7fa1bbf7a91fbf61525b799e4ee9d3377
Sujal Jethwani <sujaljethwani577@gmail.com>
2026-07-09 09:09:41 +0530

    Time: 16 ms (92.23%), Space: 134.3 MB (13.59%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -1,52 +1,52 @@
 class Solution {
     public int[] sumAndMultiply(String s, int[][] queries) {
         int n = s.length();
-        long MOD = 1_000_000_007;
 
-        long[] prefSum = new long[n + 1];
-        long[] prefHash = new long[n + 1];
-        int[] pos = new int[n + 1];
-        long[] pow10 = new long[n + 1];
+        long[] prefSum = new long[n+1];
+        long[] prefNums = new long[n+1];
+        long[] pow10 = new long[n+1];
+        int[] pos = new int[n+1];
+
+        long MOD = 1_000_000_007;
+        int nonZero = 0;
 
         pow10[0] = 1;
         for (int i = 1; i <= n; i++) {
             pow10[i] = (pow10[i - 1] * 10) % MOD;
         }
 
-        int nonZeroCount = 0;
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            if (c != '0') {
-                int digit = c - '0';
-                nonZeroCount++;
-                prefSum[nonZeroCount] = prefSum[nonZeroCount - 1] + digit;
-                prefHash[nonZeroCount] = (prefHash[nonZeroCount - 1] * 10 + digit) % MOD;
+        for(int i=0; i<n; i++) {
+            char ch = s.charAt(i);
+
+            if(ch != '0') {
+                int digit = ch - '0';
+                nonZero++;
+                prefSum[nonZero] = prefSum[nonZero - 1] + digit;
+                prefNums[nonZero] = (prefNums[nonZero - 1]* 10 + digit) % MOD;
             }
-            pos[i + 1] = nonZeroCount;
+            pos[i+1] = nonZero;
         }
 
         int[] res = new int[queries.length];
-
-        for (int i = 0; i < queries.length; i++) {
+        for(int i=0; i<queries.length; i++) {
             int l = queries[i][0];
             int r = queries[i][1];
 
-            int idxL = pos[l]; 
-            int idxR = pos[r + 1]; 
+            int idxL = pos[l];
+            int idxR = pos[r+1];
 
-            if (idxL == idxR) {
+            if(idxL == idxR) {
                 res[i] = 0;
                 continue;
             }
 
             long digitSum = prefSum[idxR] - prefSum[idxL];
-
             int numDigits = idxR - idxL;
-            long x = (prefHash[idxR] - (prefHash[idxL] * pow10[numDigits]) % MOD + MOD) % MOD;
+
+            long x = (prefNums[idxR] - (prefNums[idxL] * pow10[numDigits]) % MOD + MOD);
 
             res[i] = (int) ((x * digitSum) % MOD);
         }
-
         return res;
     }
 }
