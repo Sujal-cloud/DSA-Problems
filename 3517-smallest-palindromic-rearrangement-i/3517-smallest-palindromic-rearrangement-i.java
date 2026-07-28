0258129eeca177cf72ce9616660fdcd389e76f75
Sujal Jethwani <sujaljethwani577@gmail.com>
2026-07-28 19:21:52 +0530

    Time: 1588 ms (5.22%), Space: 48.4 MB (10.04%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -2,63 +2,59 @@ class Solution {
     static char getSmallest(Map<Character, Integer> map) {
         char smallest = Character.MAX_VALUE;
 
-        for(char ch : map.keySet()) {
-            if(map.get(ch) >= 2 && ch < smallest) {
+        for (char ch : map.keySet()) {
+            if (map.get(ch) >= 2 && ch < smallest) {
                 smallest = ch;
             }
         }
         return smallest;
     }
+
     public String smallestPalindrome(String s) {
         int n = s.length();
-        if(n == 1) {
+
+        if (n == 1) {
             return s;
         }
 
         Map<Character, Integer> map = new HashMap<>();
-        for(char ch : s.toCharArray()) {
+
+        for (char ch : s.toCharArray()) {
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
         char[] res = new char[n];
 
-        if(n % 2 == 0) {
-            int i = 0;
-            int j = n - 1;
-
-            while(i < j) {
-                char smallest = getSmallest(map);
-                res[i] = smallest;
-                res[j] = smallest;
-
-                map.put(smallest, map.get(smallest) - 2);
-                i++;
-                j--;
-            }
-        }
-        else{
+        if (n % 2 != 0) {
             char middle = ' ';
-            for(char ch : map.keySet()) {
-                if(map.get(ch) % 2 != 0) {
+
+            for (char ch : map.keySet()) {
+                if (map.get(ch) % 2 != 0) {
                     middle = ch;
                     break;
                 }
             }
 
             res[n / 2] = middle;
-            int i = 0;
-            int j = n - 1;
 
-            while(i < j) {
-                char smallest = getSmallest(map);
-                res[i] = smallest;
-                res[j] = smallest;
-
-                map.put(smallest, map.get(smallest) - 2);
-                i++;
-                j--;
-            }
+            map.put(middle, map.get(middle) - 1);
         }
+
+        int i = 0;
+        int j = n - 1;
+
+        while (i < j) {
+            char smallest = getSmallest(map);
+
+            res[i] = smallest;
+            res[j] = smallest;
+
+            map.put(smallest, map.get(smallest) - 2);
+
+            i++;
+            j--;
+        }
+
         return new String(res);
     }
 }
