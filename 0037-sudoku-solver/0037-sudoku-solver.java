f0102d5361800f1fb617cf9ad3ec5edd90917c16
Sujal Jethwani <sujaljethwani577@gmail.com>
2026-07-30 08:03:52 +0530

    Time: 238 ms (52.81%), Space: 47.1 MB (5.09%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,10 +1,10 @@
 class Solution {
-    static boolean findEmptyCell(char[][] board, int[] emptyCell) {
+    static boolean findEmptyCell(char[][] board, int[] empty) {
         for(int i=0; i<9; i++) {
             for(int j=0; j<9; j++) {
                 if(board[i][j] == '.') {
-                    emptyCell[0] = i;
-                    emptyCell[1] = j;
+                    empty[0] = i;
+                    empty[1] = j;
                     return true;
                 }
             }
@@ -13,53 +13,47 @@ class Solution {
     }
 
     static boolean isSafe(char[][] board, char charVal, int rowIdx, int colIdx) {
-        //check 3 rules
-
-        //1 value not in same row
+        //1 not in same row
         for(int col=0; col<9; col++) {
             if(board[rowIdx][col] == charVal) {
                 return false;
             }
         }
 
-        //2 value not in same col
+        //2 not in same col
         for(int row=0; row<9; row++) {
             if(board[row][colIdx] == charVal) {
                 return false;
             }
         }
 
-        //3 value not in same 3*3 grid
-        //find starting index for row and col and then simply 2D traverse
-        int startingRow = rowIdx - rowIdx%3;
-        int startingCol = colIdx - colIdx%3;
+        //3 not in same grid
+        int startRow = rowIdx - rowIdx%3;
+        int startCol = colIdx - colIdx%3;
 
         for(int i=0; i<3; i++) {
             for(int j=0; j<3; j++) {
-                if(board[startingRow + i][startingCol + j] == charVal) {
+                if(board[startRow + i][startCol + j] == charVal) {
                     return false;
                 }
             }
         }
-
-        //if every rule checked
         return true;
     }
-    static boolean helper(char[][] board) {
-        //base case 
-        // if no empty cell found -> board solved
-        int[] emptyCell = new int[2];
-        if(!findEmptyCell(board, emptyCell)) {
+    static boolean helper(char[][] board) { 
+        //base case
+        //if no empty cell left -> board completed
+        int[] empty = new int[2];
+        if(!findEmptyCell(board, empty)) {
             return true;
         }
 
-        //place values from 1 to 9 at empty cell and check if safe to place
-        int rowIdx = emptyCell[0];
-        int colIdx = emptyCell[1];
+        int rowIdx = empty[0];
+        int colIdx = empty[1];
 
+        //now put 1-9 in each cell and check if safe to place
         for(int val=1; val<=9; val++) {
             char charVal = (char)(val + '0');
-
             if(isSafe(board, charVal, rowIdx, colIdx)) {
                 board[rowIdx][colIdx] = charVal;
 
@@ -68,7 +62,7 @@ class Solution {
                     return true;
                 }
 
-                //back-tracking step for next checking undo current move
+                //backtracking -> undo step
                 board[rowIdx][colIdx] = '.';
             }
         }
