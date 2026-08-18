f4a7cd620f05992d2c59589ef4899949b989a952
Sujal Jethwani <sujaljethwani577@gmail.com>
2026-08-18 21:12:24 +0530

    Time: 10 ms (51.61%), Space: 59.4 MB (63.36%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,25 +9,56 @@
  * }
  */
 class Solution {
-    public ListNode sortList(ListNode head) {
+    static ListNode getMiddle(ListNode head) {
         if(head == null || head.next == null) return head;
 
-        List<Integer> arr = new ArrayList<>();
-        ListNode temp = head;
+        ListNode slow = head;
+        ListNode fast = head.next;
 
-        while(temp != null) {
-            arr.add(temp.val);
-            temp = temp.next;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    static ListNode merge(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+
+        ListNode dummy = new ListNode(-1);
+        ListNode res = dummy;
+
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                res.next = l1;
+                res = l1;
+                l1 = l1.next;
+            }else{
+                res.next = l2;
+                res = l2;
+                l2 = l2.next;
+            }
         }
 
-        Collections.sort(arr);
-        temp = head;
-        int i = 0;
-        while(temp != null) {
-            temp.val = arr.get(i);
-            i++;
-            temp = temp.next;
+        if(l1 != null) res.next = l1;
+        if(l2 != null) res.next = l2;
+
+        return dummy.next;
+    }
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
         }
-        return head;
+
+        ListNode middleNode = getMiddle(head);
+        ListNode leftHead = head;
+        ListNode rightHead = middleNode.next;
+        middleNode.next = null;
+
+        leftHead = sortList(leftHead);
+        rightHead = sortList(rightHead);
+
+        return merge(leftHead, rightHead);
     }
 }
