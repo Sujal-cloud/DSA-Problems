9b6f96872418e81a5fb6aa46dab3724837ee1206
Sujal Jethwani <sujaljethwani577@gmail.com>
2026-08-20 09:48:23 +0530

    Time: 70 ms (5.19%), Space: 64.6 MB (5.36%) - LeetHub

---
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        int n = nums.length;
+
+        int[][] arr = new int[n][2];
+        for(int i=0; i<n; i++) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+
+        Arrays.sort(arr, Comparator.comparingInt((int[] a) -> a[0])
+                                      .thenComparingInt(a -> a[1]));
+
+        int maxWidth = 0;
+        int minIdx = arr[0][1];
+
+        for(int i=0; i<n; i++) {
+            maxWidth = Math.max(maxWidth, arr[i][1] - minIdx);
+            minIdx = Math.min(minIdx, arr[i][1]);
+        }
+        return maxWidth;
+    }
+}
