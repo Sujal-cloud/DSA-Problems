615b20a8b8976ebf52c5aa47b9c7e2859935708c
Sujal Jethwani <sujaljethwani577@gmail.com>
2026-08-23 15:45:36 +0530

    Time: 4 ms (36.54%), Space: 43.3 MB (41.18%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        if(n == 1) return false;
+
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0; i<n; i++){
+            char ch = s.charAt(i);
+
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }else{
+                if(st.isEmpty()) return false;
+
+                char top = st.peek();
+                st.pop();
+
+                if((top == '(' && ch != ')') || (top == '[' && ch != ']') || (top == '{' && ch != '}')) {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
