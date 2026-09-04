3646c390b2ca1565341fdfe8af125bf56337198c
Sujal Jethwani <sujaljethwani577@gmail.com>
2026-09-04 23:43:22 +0530

    Time: 1 ms (98.1%), Space: 42.6 MB (63.21%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,19 @@
+class Solution {
+    static int sumDigits(int num) {
+        int sum = 0;
+        while(num != 0) {
+            sum += (num % 10);
+            num /= 10;
+        }
+        return sum;
+    }
+    public int addDigits(int num) {
+        if(num == 0) return 0;
+
+        int ans = sumDigits(num);
+        while(ans/10 != 0) {
+            ans = sumDigits(ans);
+        }
+        return ans;
+    }
+}
