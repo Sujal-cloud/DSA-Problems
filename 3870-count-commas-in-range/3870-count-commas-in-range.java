f4ea434c3b5243a8cefa7a35f7b76107881dac05
Sujal Jethwani <sujaljethwani577@gmail.com>
2026-09-08 10:32:28 +0530

    Time: 1 ms (83.65%), Space: 42.6 MB (57.08%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,24 +1,7 @@
 class Solution {
-    static int digitsCount(int n) {
-        int cntDigits = 0;
-        int temp = n;
-        while(temp != 0) {
-            temp /= 10;
-            cntDigits++;
-        }
-
-        return cntDigits;
-    }
     public int countCommas(int n) {
-        if(digitsCount(n) < 4) return 0;
+        if(n < 1000) return 0;
 
-        int commaCount = 0;
-
-        for(int i=1; i<=n; i++) {
-            if(digitsCount(i) >= 4) {
-                commaCount++;
-            }
-        }
-        return commaCount;
+        return n-999;
     }
 }
