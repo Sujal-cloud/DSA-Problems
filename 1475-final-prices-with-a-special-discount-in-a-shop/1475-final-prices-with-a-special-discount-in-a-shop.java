93e3cc431f182bc56d978c8ba6b1b10f3d958822
Sujal Jethwani <sujaljethwani577@gmail.com>
2026-09-23 19:18:23 +0530

    Time: 4 ms (47.66%), Space: 45.8 MB (9.69%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,19 +1,42 @@
 class Solution {
+    static int[] nextSmaller(int[] arr) {
+        int n = arr.length;
+
+        Stack<Integer> st = new Stack<>();
+
+        int[] ans = new int[n];
+        for(int i=n-1; i>=0; i--) {
+            int curr = arr[i];
+
+            while(!st.isEmpty() && st.peek() > curr) {
+                st.pop();
+            }
+            if(st.isEmpty()) {
+                ans[i] = -1;
+            }else{
+                ans[i] = st.peek();
+            }
+
+            st.push(curr);
+        }
+        return ans;
+    }
     public int[] finalPrices(int[] prices) {
         int n = prices.length;
 
         int[] ans = new int[n];
+        int[] nse = nextSmaller(prices);
 
         for(int i=0; i<n; i++) {
-            int dis = 0;
+            int disc = 0;
 
-            for(int j=i+1; j<n; j++) {
-                if(prices[j] <= prices[i]) {
-                    dis = prices[j];
-                    break;
-                }
+            if(nse[i] == -1) {
+                disc = 0;
+            }else{
+                disc = nse[i];
             }
-            ans[i] = prices[i] - dis;
+
+            ans[i] = prices[i] - disc;
         }
         return ans;
     }
